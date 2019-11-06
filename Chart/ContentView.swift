a58a979f2fd6f608c43feb103e4c5789965153df
ilenych <ilenych6@gmail.com>
2019-11-06 17:43:41 +0500

    Init

---
+++ Chart/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Chart
+//
+//  Created by  SENAT on 06.11.2019.
+//  Copyright © 2019 <ASi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Let's start")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
